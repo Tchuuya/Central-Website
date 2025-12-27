775697297583ad11cc129d00902ada35cc3f1dab
Tchuuya <jarupagontongpansa@gmail.com>
2025-12-27 15:14:29 +0700

    Fix main page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to the Central Website</h1>
+      <h1>Welcome to the Central Website😘.</h1>
     </div>
   );
 }
